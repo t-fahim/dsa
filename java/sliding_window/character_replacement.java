c535e6d08a98f57ef84e08e84c7555fd45603b25
tamim <t.a.fahim@outlook.com>
2025-11-17 09:37:21 +0600

    add character replacment

---
+++ java/sliding_window/character_replacement.java
@@ -0,0 +1,40 @@
+// 424. Longest Repeating Character Replacement
+// https://leetcode.com/problems/longest-repeating-character-replacement/description/
+
+import java.util.*;
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] occurance = new int[26];
+        int left = 0;
+        int ans = 0;
+        int maxOccurance = 0;
+        
+        for(int right = 0; right < s.length(); right++){
+            maxOccurance = Math.max(maxOccurance, ++occurance[s.charAt(right) - 'A']);
+            if(right - left + 1 - maxOccurance > k){
+                
+                occurance[s.charAt(left) - 'A']--;
+                left++;
+            }
+            ans = Math.max(ans, right-left + 1);
+        }
+        
+        return ans;
+    }
+}
+
+
+
+
+
+public class character_replacement {
+    public static void main (String[] args) {
+		String s = "AABABBA";
+		int k = 1;
+        Solution sol = new Solution();
+        
+        System.out.println(sol.characterReplacement(s,k));
+    }
+}
+
